e997b5fc3d7a30348598555e9d5379be8cb43ea6
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-11-17 16:12:35 -0600

    first commit as generalized windows machine

---
+++ ForBoot/piConfig.js
@@ -0,0 +1,18 @@
+exports.config = {
+  piFig: {
+    /*wifiHotspot: {
+      ssid: 'SensorServer',
+      password: 'newPass!',
+      domainName: 'swingsensors.net',
+    },*/
+    autostart: true,
+    wifi: {
+      ssid: 'SSID',
+      password: 'defaultPass',
+    },
+    /*smoothShutdown: {
+      delayTime: 1000,
+    },
+    */
+  },
+};
