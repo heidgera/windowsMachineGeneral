e23d208c1cba20b141f48e394e7ddb653c32d0d6
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2019-02-27 11:19:11 -0600

    fixing line endings

--- ForBoot/install.sh
+++ ForBoot/install.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-
-echo "\nInstalling node:"
+echo -e "\nInstalling node:"
 
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 
 sudo apt-get install --no-install-recommends xserver-xorg xinit xserver-xorg-video-fbdev xserver-xorg-video-fbturbo libxss1 libgconf-2-4 libnss3 git nodejs libgtk2.0-0 libxtst6
 
-echo "\nClone the application"
+echo -e "\nClone the application"
 
 git clone --recurse-submodules https://github.com/heidgera/windowsMachineGeneral.git
 
 cd windowsMachineGeneral
 
-echo "\nInstalling dependencies for application:"
+echo -e "\nInstalling dependencies for application:"
 
 npm i
 
-echo "\nConfiguring"
+echo -e "\nConfiguring"
 
 cd piFig
 
